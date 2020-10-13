e0b959425275c5d26e4a8174a52fc638d432af66
Dmytro Grunytsky <dima.grunitsky@gmail.com>
2020-10-13 21:07:15 +0300

    Значення z1 і z2

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Значення m");
+            double numberm = double.Parse(Console.ReadLine());
+            Console.WriteLine("Значення n");
+            double numbern = double.Parse(Console.ReadLine());
+            double z1  = numberm * numbern;
+            double z2  =numberm/numbern;
+            Console.WriteLine($"(z1){ z1}");
+            Console.WriteLine($"(z2){z2}");
+        }
+    }
+}
